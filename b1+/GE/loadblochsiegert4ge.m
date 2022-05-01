0435a3538195a01340efbf82edc374ec892ca6ff
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-05-01 16:11:47 -0400

    Need to get freq from bs.mod header On branch develop 	modified:   loadblochsiegert4ge.m

--- b1+/GE/loadblochsiegert4ge.m
+++ b1+/GE/loadblochsiegert4ge.m
@@ -10,21 +10,24 @@ function [b1, rss] = loadblochsiegert4ge(pfile, readoutFile, bsModuleFile)
 % Outputs:
 %  rssim         Root-sum-of-squares coil-combined image from first echo
 
+% Get decimation from readout module file header
 [rf,gx,gy,gz,desc,paramsint16,paramsfloat,hdr] = toppe.readmod(readoutFile);
 nx = paramsint16(1);   % image size
 ny = paramsint16(2);
 %Ry = paramsint16(4);
 decimation = paramsint16(7);
 
+% Load EPI raw data
 for echo = 1:2
     d(:,:,:,:,echo) = toppe.utils.loadepi(pfile, echo, readoutFile);  % [nx*decimation ny nz nCoils]
 end
 
-imNegFreq = toppe.utils.ift3(d(:,:,:,:,1));  % -4000 Hz
-imPosFreq = toppe.utils.ift3(d(:,:,:,:,2));  % +4000 Hz
+% Reconstruct complex coil images
+imNegFreq = toppe.utils.ift3(d(:,:,:,:,1));  % negative Bloch-Siegert frequency
+imPosFreq = toppe.utils.ift3(d(:,:,:,:,2));  % 
 bs.amp = 0.05;           % Amplitude of Fermi pulse (Gauss)
 bs.freq = [-4000 4000];  % Frequency offset of Fermi pulse (Hz)
-rf = toppe.readmod([datDir 'bs.mod']);
+rf = toppe.readmod(bsModuleFile);
 dt = 4e-6;  % RF raster time (sec)
 kbs = toppe.utils.rf.calckbs(rf, bs.freq(2), dt);
 pc = toppe.utils.phasecontrast3d(imPosFreq, imNegFreq);
